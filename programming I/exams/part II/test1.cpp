75d5e15fd0234fed7be105cf8664cb6e8cc1f923
Gld3n <robertvale301104@gmail.com>
2023-06-10 01:18:21 -0400

    Added Part II first test description

--- programming I/exams/part II/test1.cpp
+++ programming I/exams/part II/test1.cpp
@@ -1,20 +1,10 @@
-/*
-Make a stock system with the following data:
-    Product...
-    - Name
-    - Price (show in Bs. and $)
-    - Discount (0% - 99%)
-    - Amount
-IVA 0.16
-INDICATE:
-    [Hint: use a switch]
-    1. Add products
-    2. Modify products
-    3. List products
-    4. Statistics:
-        - products amount
-        - net and gross total
-        - cheapest and most expensive product
-        - larger and smaller product stack (soap: amount = 9, chairs: amount = 2)
-        - amount of products at discount
-*/
+// For activity reference, see the README file in this directory.
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int fucker;
+    cout << fucker;
+}
